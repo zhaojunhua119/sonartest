93f232f8b354693c40d607163a6608dc5de8fa5d
zhahua <zhaojunhua1988@gmail.com>
2018-03-07 15:23:32 -0800

    bad smell

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	//大噶圣诞噶圣诞
 	public static void main(String[] args) {
-		
+		System.out.println("wtf?");
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
+class A{
+private void badsmell(){}
+}
 }
