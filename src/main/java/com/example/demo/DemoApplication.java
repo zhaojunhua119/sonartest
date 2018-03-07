81cf355370e4069f50d739475faad7c2f9ecaf42
zhahua <zhaojunhua1988@gmail.com>
2018-03-07 15:12:09 -0800

    added bug

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	//大噶圣诞噶圣诞
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
